e782fa84ec12899f32b24041e4d24fd48a51cf94
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 02:05:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,5 +18,36 @@ def remove_non_strings(array)
 end 
 
 def count_elements(array)
-  array.count(name)
-end 
+  counts = Hash.new(0)
+  array.collect {|element| counts[element]+=1 }
+    counts.collect do |hash, number|
+      hash[:count] = number
+    end
+  counts.keys
+end
+
+def merge_data(keys, data)
+  keys.each do |name_hash|
+    data.each do |hash|
+      name_hash.merge!(hash[name_hash[:first_name]])
+    end
+  end
+end
+
+def find_cool(array)
+  array.select {|entry| entry if entry.has_value?("cool")}
+end
+
+def organize_schools(schools)
+  by_location = {}
+    schools.each do |school, location_hash|
+      location_hash.each do |symbol, location|
+        if by_location[location] == nil
+          by_location[location] = [school]
+        else
+          by_location[location] << school
+        end
+      end
+    end
+    by_location
+end
