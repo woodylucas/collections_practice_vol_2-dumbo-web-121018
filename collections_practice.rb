068974385089b27bbe989c86ad1675ce2e16f902
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 01:54:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
 # your code goes here
+def begins_with_r(array)
+  array.all? do |r_word|
+    r_word.downcase.chr == "r"
+  end 
+end 
+
+def contain_a(array)
+  array.select{|element| element.include?("a")}
+end 
+
+def first_wa
+  array.find? {|word| word.to_s.start_with?("wa")}
+end 
